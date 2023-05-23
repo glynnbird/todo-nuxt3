423f8ad26dcf8f55c8c5b5469aa4969115e714b8
Glynn Bird <glynn.bird@gmail.com>
2023-05-23 11:10:32 +0100

    routes

--- code/build.sh
+++ code/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 npm install
 #npx rollup -p @rollup/plugin-terser
-npx rollup --format=es --file=../functions/add_todo.js -- add_todo.js
-npx rollup --format=es --file=../functions/delete_todo.js -- delete_todo.js
-npx rollup --format=es --file=../functions/get_todo.js -- get_todo.js
-npx rollup --format=es --file=../functions/list_todos.js -- list_todos.js
+npx rollup --format=es --file=../functions/api/add_todo.js -- add_todo.js
+npx rollup --format=es --file=../functions/api/delete_todo.js -- delete_todo.js
+npx rollup --format=es --file=../functions/api/get_todo.js -- get_todo.js
+npx rollup --format=es --file=../functions/api/list_todos.js -- list_todos.js
